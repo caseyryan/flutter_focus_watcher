a4ec37e51fcdc944891a27524a0c2250dc1e9aff
CaseyRyan <konstantin.p.serov@gmail.com>
2019-09-13 19:12:12 +0700

    added contents to the example project with a usecase

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -11,9 +11,9 @@ class MyApp extends StatelessWidget {
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
-          primarySwatch: Colors.blue,
+          primarySwatch: Colors.green,
         ),
-        home: MyHomePage(title: 'Flutter Demo Home Page'),
+        home: MyHomePage(title: 'Focus watcher test'),
       ),
     );
   }
@@ -28,34 +28,96 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+  Widget _getTextField({String hintText = ''}) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        // Used instead of InputDecorator because it's way too
+        // buggy in current versions of Flutter
+        decoration: BoxDecoration(
+            border: Border.all(
+                color: Colors.green, width: 2, style: BorderStyle.solid),
+            borderRadius: BorderRadius.all(Radius.circular(6))),
+        child: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: TextField(
+            decoration: InputDecoration(
+                alignLabelWithHint: true,
+                contentPadding: EdgeInsets.all(5),
+                border: InputBorder.none,
+                hintText: hintText),
+            style:
+                TextStyle(fontSize: 20, fontWeight: FontWeight.w500, height: 1),
+          ),
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    );
+  }
+
+  Widget _getButton({bool ignorFocusWatcher = false}) {
+    var button = Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: LimitedBox(
+        maxHeight: 45,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Expanded(
+              child: OutlineButton(
+                borderSide: BorderSide(width: 2, style: BorderStyle.solid, color: Colors.green),
+                color: Colors.yellow,
+                shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(6.0)),
+                onPressed: () {
+
+                },
+                child: Text(
+                  ignorFocusWatcher ? "I will not unfocus TextField" : "But I will, hahahaha ^^",
+                  style: TextStyle(
+                    fontSize: 18
+                  ),
+                ),
+//                padding: EdgeInsets.all(8.0),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: IgnoreFocusWatcher(
-        child: FloatingActionButton(
-          onPressed: () {
-            print("FOCUS NOT CHANGING");
-          },
-          tooltip: "I don't remove focus from active text field",
-          child: Icon(Icons.not_interested),
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+
+    if (ignorFocusWatcher) {
+      return IgnoreFocusWatcher(
+        child: button
+      );
+    }
+    return button;
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          _getTextField(hintText: 'Select me'),
+          _getTextField(hintText: 'Or me. And a keyboard will stay'),
+          _getButton(),
+          _getButton(ignorFocusWatcher: true),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "Tap on a TextField to focus it. "
+              "Tap somwhere else except for 'Keep Focus' "
+              "button and another TextField to unfocus",
+            ),
+          ),
+        ],
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
